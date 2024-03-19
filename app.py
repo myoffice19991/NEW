1200dbdff4236a62c36e13e2c6390e2b98ee3d92
myoffice19991 <164043819+myoffice19991@users.noreply.github.com>
2024-03-19 22:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def game(name):
     streak = np.nan
     for i in range(2,-1,-1):
         a = random.randint(1, 3)
-        ans = st.text_input("Guess a number (Press Enter to proceed)")
+        ans = st.number_input("Guess a number (Press Enter to proceed)")
         ans = int(ans)
         if a == ans:
             j += 1
